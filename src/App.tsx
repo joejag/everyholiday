7dd5df6fcb805c514c2cffcf6c225ecc5c87f6ee
joe.wright <joe@joejag.com>
2023-02-02 21:20:28 +0000

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,87 +1,109 @@
 import './App.css'
 
+import Holidays from 'date-holidays'
 import React from 'react'
 
 import Calendar from './Calendar'
 
 const currentYear = 2023
 
-const dataSource = [
-  {
-    id: 0,
-    name: 'Google I/O',
-    location: 'San Francisco, CA',
-    startDate: new Date(currentYear, 4, 28),
-    endDate: new Date(currentYear, 4, 29),
-  },
-  {
-    id: 1,
-    name: 'Microsoft Convergence',
-    location: 'New Orleans, LA',
-    startDate: new Date(currentYear, 2, 16),
-    endDate: new Date(currentYear, 2, 19),
-  },
-  {
-    id: 2,
-    name: 'Microsoft Build Developer Conference',
-    location: 'San Francisco, CA',
-    startDate: new Date(currentYear, 3, 29),
-    endDate: new Date(currentYear, 4, 1),
-  },
-  {
-    id: 3,
-    name: 'Apple Special Event',
-    location: 'San Francisco, CA',
-    startDate: new Date(currentYear, 8, 1),
-    endDate: new Date(currentYear, 8, 1),
-  },
-  {
-    id: 4,
-    name: 'Apple Keynote',
-    location: 'San Francisco, CA',
-    startDate: new Date(currentYear, 8, 9),
-    endDate: new Date(currentYear, 8, 9),
-  },
-  {
-    id: 5,
-    name: 'Chrome Developer Summit',
-    location: 'Mountain View, CA',
-    startDate: new Date(currentYear, 10, 17),
-    endDate: new Date(currentYear, 10, 18),
-  },
-  {
-    id: 6,
-    name: 'F8 2015',
-    location: 'San Francisco, CA',
-    startDate: new Date(currentYear, 2, 25),
-    endDate: new Date(currentYear, 2, 26),
-  },
-  {
-    id: 7,
-    name: 'Yahoo Mobile Developer Conference',
-    location: 'New York',
-    startDate: new Date(currentYear, 7, 25),
-    endDate: new Date(currentYear, 7, 26),
-  },
-  {
-    id: 8,
-    name: 'Android Developer Conference',
-    location: 'Santa Clara, CA',
-    startDate: new Date(currentYear, 11, 1),
-    endDate: new Date(currentYear, 11, 4),
-  },
-  {
-    id: 9,
-    name: 'LA Tech Summit',
-    location: 'Los Angeles, CA',
-    startDate: new Date(currentYear, 10, 17),
-    endDate: new Date(currentYear, 10, 17),
-  },
-]
+let id = 0
+const holidaysFor = (color: string, holiday: Holidays) => {
+  return holiday
+    .getHolidays(currentYear)
+    .filter((h) => ['public'].includes(h.type))
+    .map((h) => {
+      return {
+        id: id++,
+        color,
+        name: h.name,
+        startDate: h.start,
+        endDate: h.start,
+      }
+    })
+}
+
+// See https://www.npmjs.com/package/date-holidays
+const usHolidays = holidaysFor('blue', new Holidays('US', { timezone: 'utc' }))
+const ukHolidays = holidaysFor(
+  'green',
+  new Holidays('GB', 'SCT', { timezone: 'utc' })
+)
+const roHolidays = holidaysFor(
+  'yellow',
+  new Holidays('RO', { timezone: 'utc' })
+)
+const ptHolidays = holidaysFor('red', new Holidays('PT', { timezone: 'utc' }))
+
+const allHolidays = [...usHolidays, ...ukHolidays, ...roHolidays, ...ptHolidays]
 
 function App() {
   return (
-    <Calendar year={2023} language="en" weekStart={1} dataSource={dataSource} />
+    <div>
+      <h2 style={{ textAlign: 'center' }}>
+        2023 holidays in US, UK, Romania and Portugal
+      </h2>
+
+      <Calendar
+        year={currentYear}
+        language="en"
+        weekStart={1}
+        dataSource={allHolidays}
+        displayHeader={false}
+      />
+
+      <h3>US Holidays</h3>
+      <ul>
+        {usHolidays.map((h) => {
+          return (
+            <li key={h.id}>
+              <>
+                {h.startDate.toDateString()}: {h.name}
+              </>
+            </li>
+          )
+        })}
+      </ul>
+
+      <h3>UK Holidays (Scotland)</h3>
+      <ul>
+        {ukHolidays.map((h) => {
+          return (
+            <li key={h.id}>
+              <>
+                {h.startDate.toDateString()}: {h.name}
+              </>
+            </li>
+          )
+        })}
+      </ul>
+
+      <h3>Romania Holidays</h3>
+      <ul>
+        {roHolidays.map((h) => {
+          return (
+            <li key={h.id}>
+              <>
+                {h.startDate.toDateString()}: {h.name}
+              </>
+            </li>
+          )
+        })}
+      </ul>
+
+      <h3>Portugal Holidays</h3>
+      <ul>
+        {ptHolidays.map((h) => {
+          return (
+            <li key={h.id}>
+              <>
+                {h.startDate.toDateString()}: {h.name}
+              </>
+            </li>
+          )
+        })}
+      </ul>
+    </div>
   )
 }
 
